c178662e34922e4248aaab9db3bb5382e0949a05
YukijZ <khoidmse173415@fpt.edu.vn>
2024-01-27 08:46:20 +0700

    React 1

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import Navigation from './components/Navigation';
+import Players from './components/Players';
+function App() {
+return (
+<div className='App'>
+<Navigation/>
+<Players/>
+</div>  
+);
+}
+export default App; 
